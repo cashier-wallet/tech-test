a5e89233ac083a392d281207d515f2aae1652787
ozgun.dundar <ozgun.dundar@gamesys.co.uk>
2022-10-25 18:45:36 +0100

    [no-dep]

--- src/main/java/com/ballys/techtest/register/model/RegisterRequest.java
+++ src/main/java/com/ballys/techtest/register/model/RegisterRequest.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 @Data
 public class RegisterRequest {
     private String username;
-    private String password;
+    private char[] password;
     private LocalDate dob;
     private String paymentCardNumber;
 }
--- src/main/java/com/ballys/techtest/register/resource/RegisterResource.java
+++ src/main/java/com/ballys/techtest/register/resource/RegisterResource.java
@@ -3,22 +3,15 @@ package com.ballys.techtest.register.resource;
 import com.ballys.techtest.register.model.RegisterRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
-@RequestMapping
-public class RegisterRequestResource {
+public class RegisterResource {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RegisterResource.class);
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(RegisterRequestResource.class);
-
-    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void register(@RequestBody RegisterRequest registerRequest) {
         LOGGER.info("NEW USER: {}", registerRequest);
