4b9d7013453dfad7cf2128cc047d962122d54d90
Thurston.Davis <Thurston.Davis@gamesys.co.uk>
2022-11-16 11:59:31 +0000

    Add Validator class for request validation.

--- src/test/java/com/ballys/techtest/rest/v1/ValidatorSmallTest.java
+++ src/test/java/com/ballys/techtest/rest/v1/ValidatorSmallTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ValidatorTest {
+class ValidatorSmallTest {
     private Validator testObj;
 
     @BeforeEach
